9daa91b06fa9508ed78468ca751eeb9700064a42
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-04-04 00:37:46 +0600

    core: return success error code during exit from main()
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/tmail.c
+++ src/tmail.c
@@ -3,5 +3,9 @@
  * Copyright (c) 2017, Alexander Kuleshov <kuleshovmail at gmail dot com>
  * This file is released under the BSD license, see the COPYING file
  */
+
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
+        return EXIT_SUCCESS;
 }
