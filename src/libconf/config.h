21b8139d88de624e69a587dcfe84e1c03fe14d33
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-04-16 00:52:05 +0600

    core: remove libconf for now
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libconf/config.h
+++
@@ -1,9 +0,0 @@
-/*
- * config.h - one header INI file parser
- *
- * Copyright (c) 2017, Alexander Kuleshov <kuleshovmail at gmail dot com>
- *
- * This file is released under the BSD license, see the COPYING file
- */
-
-#pragma once
