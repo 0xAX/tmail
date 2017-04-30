6f52db5a9b2c198827dcecb7ef4edb1818584681
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-04-30 23:41:50 +0600

    general: add .dir-locals.el for indentation in emacs
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,28 @@
+;; emacs local configuration settings for tmail source code
+
+((c-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (c-basic-offset . 8)
+  (c-file-style . "linux"))
+ (c++-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (c-basic-offset . 8)
+  (c-file-style . "linux"))
+ (emacs-lisp-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8))
+ (shell-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (sh-basic-offset . 8)
+  (sh-indentation . 8))
+ (perl-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (sh-basic-offset . 8)
+  (sh-indentation . 8))
+ (nil
+  (fill-column . 80))
+)
