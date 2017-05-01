6400d5110eddf75f424b9bcb7f6a8f8283957bce
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-05-02 00:09:59 +0600

    sys: add initial term source code file
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ src/sys/term.c
@@ -0,0 +1,11 @@
+/*
+ * term.c - low level terminal auxiliary funs.
+ *
+ * Copyright (c) 2017, Alexander Kuleshov <kuleshovmail at gmail dot com>
+ *
+ * This file is released under the BSD license, see the COPYING file
+ */
+
+void enable_raw_mode(void)
+{
+}
---
+++ src/sys/term.h
@@ -0,0 +1,14 @@
+/*
+ * term.h - low level terminal auxiliary funs.
+ *
+ * Copyright (c) 2017, Alexander Kuleshov <kuleshovmail at gmail dot com>
+ *
+ * This file is released under the BSD license, see the COPYING file
+ */
+
+#ifndef __SYS_TERM_H__
+#define __SYS_TERM_H__
+
+void enable_raw_mode(void);
+
+#endif /* __SYS_TERM_H__ */
