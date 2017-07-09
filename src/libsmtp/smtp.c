c9f43a5ed0418f64699a1a4f42cee615191fde01
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-07-09 21:24:35 +0600

    libsmtp: send test email message
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libsmtp/smtp.c
+++ src/libsmtp/smtp.c
@@ -7,11 +7,11 @@
  */
 
 #include <assert.h>
+#include <basic.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <basic.h>
 
 #include "smtp.h"
 
@@ -156,6 +156,15 @@ void send_email(int socket)
 	}
 
 	memset(response, 0, sizeof(response));
+
+	/* send data */
+	send(socket, "DATA\r\n", 6, 0);
+	if ((n = recv(socket, response, sizeof(response), 0) == -1))
+	{
+		/* TODO exit */
+	}
+	send(socket, "test message\r\n.\r\n", 17, 0);
+	send(socket, "QUIT\r\n", 6, 0);
 
 	close(socket);
 
