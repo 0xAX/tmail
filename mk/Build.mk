b9d2e608ef481652d0fa0c714fc9c1ab9dfd4ad2
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-07-20 00:39:01 +0600

    build: add {gcc,clang}-supports-flag functions
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- mk/Build.mk
+++ mk/Build.mk
@@ -6,6 +6,20 @@
 # Default standard that will be used to build tmail
 STANDARD=-std=gnu11
 
+# Set the default C compiler to use
+TMAIL_CC ?= gcc
+
+#
+# some useful routines
+#
+define gcc-supports-flag
+	$(eval $1 = $(shell echo "" | gcc $2 2>&1 | grep -q "unrecognized command"; echo "$$?"))
+endef
+
+define clang-supports-flag
+	$(eval $1 = $(shell echo "" | clang -E -c -Wfd - 2>&1 | grep -q unknown; echo "$$?"))
+endef
+
 # an architecture flags
 ifndef NON_NATIVE_BINARY
 ARCH=-march=native -mtune=native -ftree-vectorize
@@ -45,9 +59,6 @@ DEBUG_FLAGS+=-O
 DEBUG_FLAGS+=-g3
 DEBUG_FLAGS+=-fstack-protector
 
-# Set the default C compiler to use
-TMAIL_CC ?= gcc
-
 # Add compiler related warnings flags
 ifeq ($(TMAIL_CC), gcc)
 WARNINGS += -fmax-errors=2
@@ -77,12 +88,13 @@ CC_SHLIB_FLAGS=-shared -o
 # Compile an executable
 CC_EXEC_FLAGS=-o
 
-# Set of compiler flags that will be passed during build of executables
-CFLAGS_OBJS=$(WARNINGS) $(STANDARD) $(ARCH) $(MISC_FLAGS) -c -o
-# Set of compiler flags that will be passed during build of shared libraries
-CFLAGS_LIBS=-c $(WARNINGS) $(STANDARD) $(ARCH) $(MISC_FLAGS) -fpic
-
 # allow to use gnu extensions in clang 4.0.0 and above
 ifeq ($(TMAIL_CC), clang)
 CFLAGS_LIBS += -Wno-gnu
+CFLAGS_OBJS += -Wno-gnu
 endif
+
+# Set of compiler flags that will be passed during build of executables
+CFLAGS_OBJS += $(WARNINGS) $(STANDARD) $(ARCH) $(MISC_FLAGS) -c -o
+# Set of compiler flags that will be passed during build of shared libraries
+CFLAGS_LIBS += -c $(WARNINGS) $(STANDARD) $(ARCH) $(MISC_FLAGS) -fpic
