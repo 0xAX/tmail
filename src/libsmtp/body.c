951d913f5298eddf3074c3ff59fea1913ef1cb7f
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-08-01 00:03:02 +0600

    libsmtp: send minimal set of headers
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libsmtp/body.c
+++ src/libsmtp/body.c
@@ -8,29 +8,64 @@
 
 #include "smtp.h"
 
-static int send_from(int socket, message_t *message)
-{
-	char *from = NULL;
-	size_t from_len = strlen(message->from);
+#define FROM_CLAUSE "From: "
+#define FROM_CLAUSE_LEN 6
 
-	from = (char *)malloc(6 + from_len + 2 + 1);
-	if (!from)
+#define TO_CLAUSE "To: "
+#define TO_CLAUSE_LEN 4
+
+#define CC_CLAUSE "Cc: "
+#define CC_CLAUSE_LEN 4
+
+#define SUBJECT_CLAUSE "Subject: "
+#define SUBJECT_CLAUSE_LEN 9
+
+static int send_message_with_param(int socket, char *cmd, int cmd_len,
+				   char *data)
+{
+	char *msg = NULL;
+	size_t msg_len = strlen(data);
+
+	msg = (char *)malloc(cmd_len + msg_len + 2 + 1);
+	if (!msg)
 	{
 		fprintf(stderr,
 			"Error: Can't allocate memory for 'From:' field\n");
 		return 0;
 	}
-	memset(from, 0, 6 + from_len + 2 + 1);
+	memset(msg, 0, cmd_len + msg_len + 2 + 1);
 
-	/* build 'From:' line */
-	strcat(from, "From: ");
-	strncat(from, message->from, from_len);
-	strcat(from, "\r\n");
+	/* build SMTP message and send it*/
+	strcat(msg, cmd);
+	strncat(msg, data, msg_len);
+	strcat(msg, "\r\n");
+	send(socket, msg, cmd_len + msg_len + 2, 0);
 
-	/* and send it */
-	send(socket, from, 6 + from_len + 2, 0);
+	free(msg);
 
-	free(from);
+	return 1;
+}
+
+static int send_date(int socket)
+{
+	char timebuf[128];
+	time_t current_time = time(NULL);
+	struct tm *tm = localtime(&current_time);
+
+	if (!tm)
+	{
+		perror("Something going wrong during localtime() call: ");
+		return 0;
+	}
+
+	if (!strftime(timebuf, sizeof(timebuf), "%a, %d %h %Y %H:%M:%S %z", tm))
+	{
+		fprintf(stderr, "Error: can't format current date/time\n");
+		return 0;
+	}
+
+	send(socket, "Date: ", 6, 0);
+	send(socket, timebuf, strlen(timebuf), 0);
 
 	return 1;
 }
@@ -38,13 +73,59 @@ static int send_from(int socket, message_t *message)
 int send_message_body(int socket, message_t *message, char *buffer)
 {
 	int n = 0;
+	list_t *entry = NULL;
 
-	UNUSED(message);
-	if (!send_from(socket, message))
+	/* send MIME version header */
+	send(socket, "MIME-Version: 1.0\r\n", 19, 0);
+
+	/* send from header */
+	if (!send_message_with_param(socket, FROM_CLAUSE, FROM_CLAUSE_LEN,
+				     message->from))
 		return 0;
 
+	/* send 'To:' headers */
+	if (message->to)
+		for_each_list_item(
+		    message->to,
+		    entry) if (!send_message_with_param(socket, TO_CLAUSE,
+							TO_CLAUSE_LEN,
+							entry->item)) return 0;
+
+	/* send 'Cc:' headers */
+	if (message->cc)
+	{
+		send(socket, "Cc: ", 4, 0);
+
+		for_each_list_item(message->cc, entry)
+		{
+			send(socket, entry->item, strlen(entry->item), 0);
+			if (entry->next != NULL)
+				send(socket, ",", 1, 0);
+			else
+				send(socket, "\r\n", 2, 0);
+		}
+	}
+
+	/* send subject */
+	if (message->subject)
+		if (!send_message_with_param(socket, SUBJECT_CLAUSE,
+					     SUBJECT_CLAUSE_LEN,
+					     message->subject))
+			return 0;
+
+	/* send 'Date' header */
+	if (!send_date(socket))
+		return 0;
+
+	/* send utf8 by default */
+	send(socket, "Content-Type: text/plain; charset=utf-8\r\n", 41, 0);
+
+	/* send CR/LF after headers */
+	send(socket, "\r\n", 2, 0);
+
 	/* send message body */
-	send(socket, "test message 1\r\n.\r\n", 19, 0);
+	send(socket, "test message 1\r\n.\r\n",
+	     strlen("test message 1\r\n.\r\n"), 0);
 
 	if ((n = recv(socket, buffer, 1024, 0) == -1))
 	{
