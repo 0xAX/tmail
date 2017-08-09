35829e68d4a230a441f32229fd0b078fb5d5ac1a
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-08-10 00:40:48 +0600

    libsys: fix time format for non-glibc systems
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libsys/time_utils.h
+++ src/libsys/time_utils.h
@@ -9,10 +9,11 @@
 #ifndef SYS_TIME_UTILS_H
 #define SYS_TIME_UTILS_H
 
+/* In the GNU C Library, time_t is equivalent to long int */
 #if defined(__GLIBC__)
 #define TIME_FORMAT "%li"
 #else
-#define TIME_FORMAT "%ll"
+#define TIME_FORMAT "%lli"
 #endif
 
 #endif /* !SYS_TIME_UTILS_H */
