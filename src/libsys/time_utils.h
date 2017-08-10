e180a88d6a9f7f148e50571d40ef55a2a67fc3ab
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-08-11 00:04:45 +0600

    libsys: use os-specific TIME_FORMAT
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libsys/time_utils.h
+++ src/libsys/time_utils.h
@@ -12,8 +12,15 @@
 /* In the GNU C Library, time_t is equivalent to long int */
 #if defined(__GLIBC__)
 #define TIME_FORMAT "%li"
-#else
+#elif (defined(__APPLE__) && defined(__MACH__))
+#define TIME_FORMAT "%li"
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+#define TIME_FORMAT "%li"
+#elif defined(__OpenBSD__)
 #define TIME_FORMAT "%lli"
+#else
+/* assume musl*/
+#define TIME_FORMAT "%li"
 #endif
 
 #endif /* !SYS_TIME_UTILS_H */
