cc538a684d5e75a1ee4847889f2ae3ae0339974a
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-09-17 00:50:36 +0600

    tls: add tls_header_t struct
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libtls/tls.h
+++ src/libtls/tls.h
@@ -20,6 +20,13 @@
 #define HANDSHAKE_MSG 22
 #define APPLICATION_DATA_MSG 23
 
+typedef struct
+{
+	byte_t type;
+	unsigned short version;
+	unsigned short length;
+} tls_header_t;
+
 int start_tls_negotiation(socket_t socket);
 
 #endif
