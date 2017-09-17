af07c8c40b4972ad5f589ad69af32acb48caea43
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-09-17 18:05:32 +0600

    tls: move handshake_random() to the tls_utils.c
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libtls/handshake.c
+++ src/libtls/handshake.c
@@ -9,24 +9,3 @@
  */
 
 #include "tls.h"
-
-/**
- * handshake_random function returns 32-byte random
- * sequence for handshake.
- *
- * In a failure case it returns `NULL` value.
- *
- * https://tools.ietf.org/html/rfc5246#section-7.4.1.2
- */
-byte_t *handshake_random(void)
-{
-	byte_t *random = malloc(sizeof(byte_t) * 32);
-
-	if (!random)
-		return NULL;
-
-	for (byte_t i = 0; i < 32; i++)
-		random[i] = (byte_t)rand();
-
-	return random;
-}
