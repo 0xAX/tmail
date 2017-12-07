2b74df1101db45aaeec0850eff7039635a0f7ece
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-12-08 00:07:06 +0600

    build: exit 0 from script
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,5 +5,5 @@ PREFIX="${HOME}/opt"
 wget -P download-cache/ "https://www.openssl.org/source/openssl-1.1.0g.tar.gz"
 tar zxf "download-cache/openssl-1.1.0g.tar.gz"
 cd openssl-1.1.0g
-./config shared --prefix="${PREFIX}" --openssldir="${PREFIX}" -DPURIFY && make -j4 all install_sw
-
+./config shared --prefix="${PREFIX}" --openssldir="${PREFIX}" -DPURIFY && make all install_sw
+exit 0
