1c89944895da8f4983823eaebfcc641bfd5c8f41
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-01-17 00:28:03 +0600

    test: stub for configuration tests
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ src/tests/config_test.c
@@ -0,0 +1,15 @@
+/*
+ * mime_test.c - some tests for MIME types
+ *
+ * Copyright (c) 2017, Alexander Kuleshov <kuleshovmail at gmail dot com>
+ *
+ * This file is released under the BSD license, see the COPYING file
+ */
+
+#include <smtp.h>
+#include <config.h>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
