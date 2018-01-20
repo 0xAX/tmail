6f4353d5e17b5ff22dcb0b8cdf20146f84f68c0d
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-01-21 00:17:51 +0600

    fix libs order

--- mk/Build.mk
+++ mk/Build.mk
@@ -138,6 +138,6 @@ endif
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
 	ifndef SSL_DISABLED
-		OSX_FLAGS=-I/usr/local/opt/openssl/include
+		OSX_FLAGS=-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib
 	endif
 endif
