a0ae899f2572dd61bba2e033cf9ec4d7956bda87
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-05-18 23:33:53 +0600

    base64: improve base64_encoding test
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/libencoding/base64_test.c
+++ src/libencoding/base64_test.c
@@ -10,28 +10,49 @@
 
 #include <stdio.h>
 
+base64_data_t *base64_encode_test(const char *str)
+{
+	base64_data_t *result = base64_encode((char *)str, strlen(str), 0);
+
+	return result;
+}
+
 int main(int argc, char *argv[])
 {
-	char *test_1 = "a";
-	base64_data_t *result_test_1 = base64_encode(test_1, 1, 0);
-	printf("result_test_encoding_1 %s\n", result_test_1->data);
+	/*
+	 * the first argument is a string to encode,
+	 * the second argument is the result
+	 */
+	if (argv[1] && argv[2])
+	{
+		int res;
+		base64_data_t *result = base64_encode_test(argv[1]);
 
-	char *test_2 = "ab";
-	base64_data_t *result_test_2 = base64_encode(test_2, 2, 0);
-	printf("result_test_encoding_2 %s\n", result_test_2->data);
+		printf("%s", result->data);
+	}
+	else
+	{
+		char *test_1 = "a";
+		base64_data_t *result_test_1 = base64_encode(test_1, 1, 0);
+		printf("result_test_encoding_1 %s\n", result_test_1->data);
 
-	char *test_3 = "abc";
-	base64_data_t *result_test_3 = base64_encode(test_3, 3, 0);
-	printf("result_test_encoding_3 %s\n", result_test_3->data);
+		char *test_2 = "ab";
+		base64_data_t *result_test_2 = base64_encode(test_2, 2, 0);
+		printf("result_test_encoding_2 %s\n", result_test_2->data);
 
-	char *test_4 = "abcd";
-	base64_data_t *result_test_4 = base64_encode(test_4, 4, 0);
-	printf("result_test_encoding_4 %s\n", result_test_4->data);
+		char *test_3 = "abc";
+		base64_data_t *result_test_3 = base64_encode(test_3, 3, 0);
+		printf("result_test_encoding_3 %s\n", result_test_3->data);
 
-	char *test_5 = "The test of the long long, very long line, a little "
-		       "bit longer than seventy six bytes length";
-	base64_data_t *result_test_5 = base64_encode(test_5, strlen(test_5), 0);
-	printf("result_test_encoding_5 %s\n", result_test_5->data);
+		char *test_4 = "abcd";
+		base64_data_t *result_test_4 = base64_encode(test_4, 4, 0);
+		printf("result_test_encoding_4 %s\n", result_test_4->data);
+
+		char *test_5 = "The test of the long long, very long line, a little "
+			"bit longer than seventy six bytes length";
+		base64_data_t *result_test_5 = base64_encode(test_5, strlen(test_5), 0);
+		printf("result_test_encoding_5 %s\n", result_test_5->data);
+	}
 
 	return 0;
 }
--- src/libencoding/run_tests.pl
+++ src/libencoding/run_tests.pl
@@ -7,6 +7,7 @@
 
 use strict;
 use warnings;
+use MIME::Base64;
 use Term::ANSIColor 2.00 qw(:pushpop);
 
 require "../../scripts/test_diff.pl";
@@ -22,7 +23,6 @@ result_test_encoding_3 YWJj
 result_test_encoding_4 YWJjZA==
 result_test_encoding_5 VGhlIHRlc3Qgb2YgdGhlIGxvbmcgbG9uZywgdmVyeSBsb25nIGxpbmUsIGEgbGl0dGxlIGJpdCBsb25nZXIgdGhhbiBzZXZlbnR5IHNpeCBieXRlcyBsZW5ndGg=
 EOF
-
 
 print "Running: libencoding tests:\n";
 
@@ -47,5 +47,44 @@ for my $test (@libencoding_tests) {
 	    print POPCOLOR;
 	}
 }
+
+#
+# Generate 10 random strings and pass it to base64 util
+# and base_64_test to test them.
+#
+print "Running libencoding fuzzing tests:\n";
+my @chars = ("A".."Z", "a".."z");
+for (my $i=0; $i <= 9; $i++) {
+    my $result = '';
+    my $orig_str = '';
+    my $base64_str = '';
+
+    # Generate random string
+    $orig_str .= $chars[rand @chars] for 1..int(rand(200));
+    # Encode it with base64
+    $base64_str .= encode_base64($orig_str, "\r\n");
+
+    # Perl adds \r\n to the end of an encoded string everytime,
+    # let's remove it for a test
+    chop $base64_str;
+    chop $base64_str;
+
+    # pass them to base64_test
+    $result = `./base64_test $orig_str \'$base64_str\'`;
+    chomp $result;
+
+    if ($result ne $base64_str) {
+	print "Pass string: $orig_str\n";
+	print "Expected: " . $base64_str . "\n";
+	print "Got:      " . $result . "\n\n";
+
+	exit 1;
+    }
+
+}
+
+print "base64_test...............";
+print PUSHCOLOR GREEN "ok\n";
+print POPCOLOR;
 
 exit 0;
