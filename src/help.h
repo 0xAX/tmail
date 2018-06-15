aced955074226655cefae32a3c992654f841e57d
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-06-16 01:02:18 +0600

    general: remove unused src/help.h
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/help.h
+++
@@ -1,7 +0,0 @@
-/*
- * tmail.c - Main executable of tmail util.
- *
- * Copyright (c) 2017-2018, Alexander Kuleshov <kuleshovmail at gmail dot com>
- *
- * This file is released under the BSD license, see the COPYING file
- */
