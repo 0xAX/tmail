a1b4cd91d338f13fcc2594206338b03ffe52206f
Alexander Kuleshov <kuleshovmail@gmail.com>
2018-06-17 00:10:26 +0600

    general: update .gitignore, so it will not ignore headers
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

---
+++ include/tmail/help.h
@@ -0,0 +1,17 @@
+/*
+ * help.h - Provides auxiliary routines for tmail help.
+ *
+ * Copyright (c) 2017-2018, Alexander Kuleshov <kuleshovmail at gmail dot com>
+ *
+ * This file is released under the BSD license, see the COPYING file
+ */
+
+#ifndef TMAIL_HELP_H
+#define TMAIL_HELP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_libs(void);
+
+#endif
